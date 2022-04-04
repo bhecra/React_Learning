fb45c959eff08c72c655c27fe1eaf8340dae9d27
burb800024 <burb800024@siigo.com>
2022-04-04 17:14:27 -0500

    boton de reset implementado

--- 03-counter-app/src/tests/CounterApp.test.js
+++ 03-counter-app/src/tests/CounterApp.test.js
@@ -37,4 +37,17 @@ describe('Pruebas en <CounterApp/> ', () => {
 
     expect(textoh).toBe(`${9}`);
   });
+
+  test('debe establecer el valor por defecto con el boton reset', () => {
+    const wraper = shallow(<CounterApp value={105} />);
+
+    wraper.find('button').at(0).simulate('click');
+    wraper.find('button').at(0).simulate('click');
+
+    wraper.find('button').at(1).simulate('click');
+
+    const textoh = wraper.find('h2').text().trim();
+
+    expect(textoh).toBe(`105`);
+  });
 });
