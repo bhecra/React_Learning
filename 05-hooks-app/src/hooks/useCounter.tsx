65a998fa5276741a0d47b045dc9f57c5995cc9eb
bhecra <ingchristianburbano@gmail.com>
2022-04-11 09:07:54 -0500

    bases react pro

--- 05-hooks-app/src/hooks/useCounter.tsx
+++ 05-hooks-app/src/hooks/useCounter.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-export const useCounter = (initialState = 10) => {
+export const useCounter = (initialState:number = 10) => {
 
   const [state, setState] = useState(initialState);
 
